62de1add3275d724c5a414aecb86e7daedfec4ba
awb13578 <awb13578@gmail.com>
2026-08-03 14:39:39 +0700

    dev_mcu_gpio

---
+++ newproject/FW_Lib/Src/mcu_gpio.c
@@ -0,0 +1,32 @@
+#include "mcu_gpio.h"
+
+void mcu_gpio_set(gpio_ctx_t *ctx)
+{
+    HAL_GPIO_WritePin((GPIO_TypeDef *)ctx->hw_cfg.port, ctx->hw_cfg.pin, GPIO_PIN_SET);
+}
+
+void mcu_gpio_clear(gpio_ctx_t *ctx)
+{
+	HAL_GPIO_WritePin((GPIO_TypeDef *)ctx->hw_cfg.port, ctx->hw_cfg.pin, GPIO_PIN_RESET);
+}
+
+void mcu_gpio_toggle(gpio_ctx_t *ctx)
+{
+    HAL_GPIO_TogglePin((GPIO_TypeDef *)ctx->hw_cfg.port, ctx->hw_cfg.pin);
+}
+
+uint32_t mcu_gpio_read(gpio_ctx_t *ctx)
+{
+    GPIO_PinState pin_state = HAL_GPIO_ReadPin((GPIO_TypeDef *)ctx->hw_cfg.port, ctx->hw_cfg.pin);
+
+    return (pin_state == GPIO_PIN_SET) ? 1 : 0;
+}
+
+void mcu_gpio_init(gpio_ctx_t *ctx)
+{
+    if (ctx == NULL || ctx->hw_cfg.port == NULL)
+    {
+        return;
+    }
+
+}
