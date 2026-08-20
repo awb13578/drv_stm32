6bd03716dd6c30157500451778228266c0bffc92
awb13578 <awb13578@gmail.com>
2026-08-20 15:29:43 +0700

    chore, fix (drv_uart) : sửa lỗi và thêm pull up cho RX

--- drv_stm32/Core/Src/stm32g0xx_hal_msp.c
+++ drv_stm32/Core/Src/stm32g0xx_hal_msp.c
@@ -273,12 +273,19 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     PB9     ------> USART3_RX
     PB8     ------> USART3_TX
     */
-    GPIO_InitStruct.Pin = UART_RX_LLC_Pin|UART_TX_LLC_Pin;
+    GPIO_InitStruct.Pin = UART_RX_LLC_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF4_USART3;
+    HAL_GPIO_Init(UART_RX_LLC_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = UART_TX_LLC_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF4_USART3;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(UART_TX_LLC_GPIO_Port, &GPIO_InitStruct);
 
     /* USART3 DMA Init */
     /* USART3_RX Init */
--- drv_stm32/FW_Lib/Src/mcu_stm32g0xx_uart.c
+++ drv_stm32/FW_Lib/Src/mcu_stm32g0xx_uart.c
@@ -8,7 +8,6 @@ static void mcu_uart_send_message (uart_ctx_t *ctx) {
 	if (ctx && ctx->hw_cfg.huart) {
 		HAL_UART_Transmit_DMA(ctx->hw_cfg.huart, ctx->sw_data.tx_buffer, ctx->sw_data.tx_size);
 		ctx->sw_data.flag_tx_busy = 1;
-//		if (HAL_UART_Transmit_DMA(ctx->hw_cfg.huart, ctx->sw_data.tx_buffer, ctx->sw_data.tx_size) != HAL_OK) ctx->sw_data.flag_tx_busy = 0;
 	}
 }
 
@@ -31,8 +30,6 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
 	if (uart_ctx == NULL || !uart_ctx->hw_cfg.huart) return;
 	uart_ctx->sw_data.rx_size = Size;
 	drv_uart_rx_callback(uart_ctx->sw_data.rx_size);
-	// Clear UART flags
-	__HAL_UART_CLEAR_FLAG(huart, UART_CLEAR_OREF | UART_CLEAR_NEF | UART_CLEAR_PEF | UART_CLEAR_FEF | UART_CLEAR_IDLEF);
 	HAL_UARTEx_ReceiveToIdle_DMA(uart_ctx->hw_cfg.huart, uart_ctx->sw_data.rx_buffer, sizeof(uart_ctx->sw_data.rx_buffer));
 }
 
