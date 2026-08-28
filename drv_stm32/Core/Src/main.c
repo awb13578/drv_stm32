4d874fee4a3f8163b427c518c129879285511a91
awb13578 <awb13578@gmail.com>
2026-08-28 17:00:34 +0700

    chore (drv_can) :  bật config lọc trong stm32mx

--- drv_stm32/Core/Src/main.c
+++ drv_stm32/Core/Src/main.c
@@ -330,7 +330,7 @@ static void MX_FDCAN2_Init(void)
   hfdcan2.Init.DataSyncJumpWidth = 1;
   hfdcan2.Init.DataTimeSeg1 = 1;
   hfdcan2.Init.DataTimeSeg2 = 1;
-  hfdcan2.Init.StdFiltersNbr = 0;
+  hfdcan2.Init.StdFiltersNbr = 1;
   hfdcan2.Init.ExtFiltersNbr = 0;
   hfdcan2.Init.TxFifoQueueMode = FDCAN_TX_QUEUE_OPERATION;
   if (HAL_FDCAN_Init(&hfdcan2) != HAL_OK)
